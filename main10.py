cb41ec928dbd60ca60a50c76cfa46f38d4340ed8
Jonah Lazarz <raponod@gmail.com>
2019-08-27 10:47:27 -0400

    Completed the Assignment

--- main10.py
+++ main10.py
@@ -6,25 +6,25 @@ utils.check_version((3,7))          # make sure we are running at least Python 3
 utils.clear()                       # clear the screen
 
 
-print('Greetings!')
-colors = ['red','orange','yellow','green','blue','violet','purple']
-play_again = ''
-best_count = sys.maxsize            # the biggest number
-while (play_again != 'n' and play_again != 'no'):
-    match_color = random.choice(colors)
-    count = 0
-    color = ''
-    while (color != match_color):
+print('Greetings!') #There is a nice greeting
+colors = ['red','orange','yellow','green','blue','violet','purple'] #These are the colors involved
+play_again = '' # the play again is for when you succeed or fail.
+best_count = sys.maxsize            # the biggest number 
+while (play_again != 'n' and play_again != 'no'): #If you type n or no, the game ends
+    match_color = random.choice(colors) #The favorite color is random.
+    count = 0 #This is the beginnig tick of the counter of attempts.
+    color = '' #The code starts here for your response.
+    while (color != match_color): #If you guess the assinged color the following happens.
         color = input("\nWhat is my favorite color? ")  #\n is a special code that adds a new line
-        color = color.lower().strip()
-        count += 1
-        if (color == match_color):
-            print('Correct!')
-        else:
-            print('Sorry, try again. You have guessed {guesses} times.'.format(guesses=count))
-    print('\nYou guessed it in {0} tries!'.format(count))
-    if (count < best_count):
-        print('This was your best guess so far!')
-        best_count = count
-    play_again = input("\nWould you like to play again? ").lower().strip()
-print('Thanks for playing!')
+        color = color.lower().strip() #Allows for capitals and spaces
+        count += 1 #Adds a tick to the counter
+        if (color == match_color): #Guess the assigned color
+            print('Correct!') #You win
+        else: #If you dont win
+            print('Sorry, try again. You have guessed {guesses} times.'.format(guesses=count)) #You guessed wrong and it tells you how many times that you tried.
+    print('\nYou guessed it in {0} tries!'.format(count)) #How many times you guessed
+    if (count < best_count): #If it takes less tries than before, it tells you that you did better.
+        print('This was your best guess so far!') #<-'
+        best_count = count #if the best count is the same, nothing happens.
+    play_again = input("\nWould you like to play again? ").lower().strip() #It asks if you want to play again and you say a color.
+print('Thanks for playing!') # A polite goodbye.
